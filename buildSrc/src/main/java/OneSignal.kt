b80c8c2bd2a722e6651ca55b752492677ec2573f
Shaik Jaleel <shaikjaleel270@gmail.com>
2020-04-01 17:11:20 +0530

    SJWalls: Enable Firebase and OneSignal
    Signed-off-by: Shaik Jaleel <shaikjaleel270@gmail.com>

--- buildSrc/src/main/java/OneSignal.kt
+++ buildSrc/src/main/java/OneSignal.kt
@@ -1,6 +1,6 @@
 @file:Suppress("unused")
 
 object OneSignal {
-    const val appId = "965921b5-91a2-45b8-a960-a9f28948a781"
-    const val googleProjectNumber = "191533712411"
+    const val appId = "5f3ec956-cacf-41b1-869e-b53f33f9e4c8"
+    const val googleProjectNumber = "986994863557"
 }
