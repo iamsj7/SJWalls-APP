c0b235c3bd997835ee6f88c79bb2f1be8136f97d
Shaik Jaleel <shaikjaleel270@gmail.com>
2020-04-01 16:53:56 +0530

    SJWalls: Enable Notifications
    Signed-off-by: Shaik Jaleel <shaikjaleel270@gmail.com>

--- app/src/main/kotlin/dev/jahir/frames/app/MyApplication.kt
+++ app/src/main/kotlin/dev/jahir/frames/app/MyApplication.kt
@@ -8,12 +8,10 @@ import dev.jahir.frames.ui.FramesApplication
 class MyApplication : FramesApplication() {
     override fun onCreate() {
         super.onCreate()
-        // TODO: Remove comment marks to enable
-        /*
         OneSignal.startInit(this)
             .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
             .unsubscribeWhenNotificationsAreDisabled(true)
             .init()
-         */
+        
     }
 }
--- app/src/main/kotlin/dev/jahir/frames/app/NotificationService.kt
+++ app/src/main/kotlin/dev/jahir/frames/app/NotificationService.kt
@@ -1,6 +1,5 @@
 package dev.jahir.frames.app
 
-/* TODO: Remove comment marks to enable
 import com.onesignal.NotificationExtenderService
 import com.onesignal.OSNotificationReceivedResult
 import dev.jahir.frames.extensions.prefs
@@ -9,4 +8,3 @@ class NotificationService : NotificationExtenderService() {
     override fun onNotificationProcessing(notification: OSNotificationReceivedResult?): Boolean =
         !prefs.notificationsEnabled
 }
-*/
